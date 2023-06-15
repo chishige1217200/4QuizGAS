ca82e875623fa05bb323ee3d8004d71833ff86cb
chishige1217200 <daichicchi01@gmail.com>
2023-06-15 23:43:36 +0900

    Update: Comment Del: 未使用処理

--- code.js
+++ code.js
@@ -1,10 +1,6 @@
 // テンプレコード
-function getTable() {
-  return SpreadsheetApp.getActiveSheet().getDataRange().getValues();
-}
 
 function doGet() {
-  // 先につくった HTML のファイル名を入れます。template.html なのであれば template。.html は抜いてください
   const fileName = "index";
   return HtmlService.createTemplateFromFile(fileName).evaluate();
 }
@@ -13,20 +9,16 @@ function include(filename) {
   return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
 
-function printTable() {
-  console.log(getTable());
-}
-
 // 自作コード
-
-// 存在するデータの範囲内で乱数を返す関数
-// 問題IDを乱数で決める
-// return 1からシート最終行までの乱数を返す
+/**
+ * 存在するデータ範囲内で乱数値を返す関数
+ * @returns {int} 1以上コンテンツが含まれている最後の行の位置以下の乱数値
+ */
 function getRnd() {
   function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
   }
 
   let id = getRandomIntInclusive(1, getMaxRowCount());
@@ -34,30 +26,44 @@ function getRnd() {
   return id;
 }
 
+/**
+ * アクティブシートのコンテンツが含まれている最後の行の位置を返します．
+ * @returns {int} コンテンツが含まれている最後の行の位置
+ */
 function getMaxRowCount() {
   return SpreadsheetApp.getActiveSheet().getLastRow();
 }
 
-// 問題文を返す
-// 諸都合により，idと連結して返す
+/**
+ * 
+ * @param {int} id 出題する問題ID
+ * @returns {[id, string]} 出題する問題のIDと問題文
+ */
 function getQuizDescription(id) {
   let sheet = SpreadsheetApp.getActiveSheet();
 
   return [id, sheet.getRange(id, 2).getValue()];
 }
 
-// 問題の選択肢を返す(ボタン生成用)
+/**
+ * 問題の選択肢を4つ返します．
+ * @param {int} id 正解選択肢の問題ID
+ * @returns {[[string, int]]} 4つの選択肢とその問題IDを含む配列
+ */
 function getQuizOptions(id) {
   let sheet = SpreadsheetApp.getActiveSheet();
   let optionsList = [[sheet.getRange(id, 1).getValue(), id]];
 
+  // 間違った選択肢を3つ追加する
   let count = 0;
   while (count < 3) {
     let tmpNum = getRnd();
     let newOption = [sheet.getRange(tmpNum, 1).getValue(), tmpNum];
 
+    // 比較条件が複数あるため，includesメソッドではなくsomeメソッドを用いる必要がある
     let some = optionsList.some(o => o[0] === newOption[0] && o[1] === newOption[1]);
 
+    // リストに含まれていない値であればリストに追加する
     if (!some) {
       optionsList.push(newOption);
       count++;
@@ -70,6 +76,11 @@ function getQuizOptions(id) {
   return optionsList;
 }
 
+/**
+ * 渡された配列の要素の順番をシャッフルします．
+ * @param {[]} array シャッフルしたい配列
+ * @returns {[]} シャッフルした配列（参照渡しなので元変数の参照をしても良い）
+ */
 function arrayShuffle(array) {
   for (let i = array.length - 1; 0 < i; i--) {
     // 0〜(i+1)の範囲で値を取得
